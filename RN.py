6cd40c2da01b41bfdac5d420dcdd0968edb7efc7
GoxShiva <151367976+GoxShiva@users.noreply.github.com>
2025-07-18 17:14:54 +0530

    Update RN.py

--- RN.py
+++ RN.py
@@ -13,32 +13,21 @@ if not os.path.exists(update_flag):
 
     try:
         if sys.argv[1] == 'update':
-            os.system('rm -rf Sarfraz.so Sarfraz32.os')
+            os.system('rm -rf Sarfraz.so')
     except:
         pass
 
     bit = platform.architecture()[0]
+
     if bit == '64bit':
         if not os.path.isfile('Sarfraz.so'):
             os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
             os.system('clear')
             print('\n\033[1;34mCracking \033[1;33mSSB \033[1;31mSequrity.......\n\n\n')
             print('\033[1;32mDONE ENJOY :)')
-            else:
-            	import Sarfraz
-  
-    if bit == '32bit':
-        if not os.path.isfile('Sarfraz.so'):
-            os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
-            os.system('clear')
-            print('\n\033[1;34mCracking \033[1;33mSSB \033[1;31mSequrity.......\n\n\n')
-            print('\033[1;32mDONE ENJOY :)')
-            else:
-            	import Sarfraz
 
     # Create the update flag file to indicate that the update has been done
     open(update_flag, 'w').close()
 
 import Sarfraz
-else:
-	import Sarfraz32
+Sarfraz.random_number2()
