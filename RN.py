d318541d475aed7b6e1eb49a984a41fba9e14e7b
GoxShiva <151367976+GoxShiva@users.noreply.github.com>
2025-07-18 17:37:48 +0530

    Update RN.py

--- RN.py
+++ RN.py
@@ -29,5 +29,5 @@ if not os.path.exists(update_flag):
     # Create the update flag file to indicate that the update has been done
     open(update_flag, 'w').close()
 
-import Sarfraz
+import Dump32
 Sarfraz.random_number2()
